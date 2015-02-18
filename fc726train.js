dedefd425f59f627d0e6e359cebac2f0a938ec06
Dale McCrary <dmccrary2@gmail.com>
2015-02-17 18:01:15 -0700

    - Added the base files and some basic descriptions

---
+++ fc726train.js
@@ -0,0 +1,19 @@
+/**
+ * The purpose of this file is to add a new "Generate Certificate" link to the Training tab so that a certificate can be printed when the bookmarklet is engaged
+ * The bookmarklet is the enging that runs these functions.
+ * 
+ * This script will pull in the certificate.html and populate it with applicable data.
+ * The end user will then be able to print it.
+ * 
+ * All of the names were pre-pended with fc726_ to try and make unique function names so as not to
+ * blow up anything on the page
+ */
+
+
+/**
+ * Add a link to every section in training labeled "Generate Certificate"
+ */
+function fc726_add_link()
+{
+    
+}
